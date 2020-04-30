68e5b4d0a19696286866f389f476e239134ccd41
Justin Smith <mistahj@gmail.com>
2020-04-29 22:47:45 -0700

    updates to games file

--- dojo_assignments/Web_Fundamentals/js/games/games.js
+++ dojo_assignments/Web_Fundamentals/js/games/games.js
@@ -1,30 +1,41 @@
-$("document").ready(function(){
-var world = [
-    [2,2,2,2,2,2,2,2,2,2],
-    [2,1,1,2,1,1,1,1,1,2],
-    [2,1,1,2,1,2,2,2,1,2],
-    [2,1,1,2,1,2,1,2,1,2],
-    [2,1,1,2,1,2,1,2,1,2],
-    [2,1,1,2,2,2,1,2,1,2],
-    [2,1,1,1,1,1,1,2,1,2],
-    [2,1,1,1,1,1,1,1,1,2],
-    [2,2,2,2,2,2,2,2,2,2]
-];
-
+function createPage(){
+    var world = [
+        [2,2,2,2,2,2,2,2,2,2],
+        [2,1,1,2,1,1,1,1,1,2],
+        [2,1,1,2,1,2,2,2,1,2],
+        [2,1,1,2,1,2,1,2,1,2],
+        [2,1,1,2,1,2,1,2,1,2],
+        [2,1,1,2,2,2,1,2,1,2],
+        [2,1,1,1,1,1,1,2,1,2],
+        [2,1,1,1,1,1,1,1,1,2],
+        [2,2,2,2,2,2,2,2,2,2]
+    ];
+    
     var output = '';
 
     for(var i=0; i<world.length; i++){
-        output += "\n<div class='row'>";
+        output += "<div class='row'>";
         for(var j=0; j<world[i].length; j++){
             if(world[i][j] === 2){
-            output += "\n\t<div.class='brick'></div>";
+            output += "<div class='brick'>" + "</div>";
             } else if(world[i][j] === 1){
-                output += "\n\t<div.class='coin'></div>";
+                output += "<div class='coin'>" + "</div>";
             } else if(world[i][j] === 0){
-                output += "\n\t<div.class='empty'></div>";
+                output += "<div class='empty'>" + "</div>";
             }
         }
-        output += "\n</div>";
+        output += "</div>";
     }
-    $(".wrapper").html(output);
+    console.log(output);
+    $(".wrapper").append(output);
+
+};
+
+$("document").keypress(function(){
+    console.log(e);
+});
+
+$("document").ready(function(){
+    createPage();
+
 });
