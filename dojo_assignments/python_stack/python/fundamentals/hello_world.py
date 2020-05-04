c0517aa60e9d54cbdc5dcc3423e5b1a0e97040d3
Justin Smith <mistahj@gmail.com>
2020-05-03 22:28:57 -0700

    adding new folder, for the python stack of coding dojo program

---
+++ dojo_assignments/python_stack/python/fundamentals/hello_world.py
@@ -0,0 +1,15 @@
+# 1. TASK: print "Hello World"
+print("Hello World")
+# 2. print "Hello Noelle" with the name in a variable
+name = "Justin"
+print("Hello",name+"!")
+print("Hello " + name + "!")
+# 3. print "Hello 42!"
+faveNum = 6
+print("Hello", str(faveNum)+"!")
+print("Hello " + str(faveNum) +"!")
+# 4. print "I love to eat sushi and pizza." With the food in variables
+food1 = "sushi"
+food2 = "pizza"
+print("I love to eat {} and {}.".format(food1, food2))
+print(f"I love to eat {food1} and {food2}.")
---
+++ dojo_assignments/python_stack/python/fundamentals/test.py
@@ -0,0 +1,5 @@
+x = 50
+if x > 50:
+    print("bigger than 50")
+else:
+    print("smaller than 50")
