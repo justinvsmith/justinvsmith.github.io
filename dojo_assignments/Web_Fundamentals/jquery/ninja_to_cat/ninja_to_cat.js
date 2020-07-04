153bc61852ff055ae008a489e1ddeba3a18e0f08
Justin Smith <smith.justinv@gmail.com>
2020-07-03 18:02:48 -0700

    restarting things with this repository to use more correctly

--- dojo_assignments/Web_Fundamentals/jquery/ninja_to_cat/ninja_to_cat.js
+++
@@ -1,7 +0,0 @@
-$("document").ready(function(){
-    $("img").click(function(){
-       console.log("data-alt-src value is", $(this).attr("data-alt-src")); 
-       var newImg = $(this).attr("data-alt-src");
-        $(this).attr("src", newImg);
-    });
-});
--- dojo_assignments/python_stack/python/fundamentals/test.py
+++
@@ -1,5 +0,0 @@
-x = 50
-if x > 50:
-    print("bigger than 50")
-else:
-    print("smaller than 50")
